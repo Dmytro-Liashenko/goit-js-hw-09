a85d9f6092fad2e63f7ce9a9187054d3b188970b
Dmytro_L <dimka4545452@gmail.com>
2025-10-07 18:02:08 +0200

    ready work

--- src/js/2-form.js
+++ src/js/2-form.js
@@ -1,1 +1,48 @@
-console.log("hello");
+
+const formData = { email: "", message: "" }
+
+const form = document.querySelector(".feedback-form");
+
+const STORAGE_KEY = "feedback-form-state";
+form.addEventListener("submit", onHandlerSubmit)
+form.addEventListener("input", onHandlerInput);
+populateForm()
+function onHandlerInput(event){
+    const fieldName = event.target.name
+    const fieldValue = event.target.value
+    formData[fieldName] = fieldValue
+    localStorage.setItem(STORAGE_KEY, JSON.stringify(formData))
+}
+function populateForm(){
+    const messageUser = localStorage.getItem(STORAGE_KEY);
+    if(!messageUser){
+        return
+    }
+    const parse = JSON.parse(messageUser)
+
+    if(parse.email){
+        form.querySelector(`input[name="email"]`).value = parse.email
+        formData.email = parse.email
+    }
+    if(parse.message){
+        form.querySelector(`textarea[name="message"]`).value = parse.message;
+        formData.message = parse.message
+    }
+}
+function onHandlerSubmit(event){
+event.preventDefault();
+const trimmedEmail = formData.email.trim();
+const trimmedMessage = formData.message.trim();
+if(!trimmedEmail || !trimmedMessage){
+    alert("Fill please all fields");
+    return
+}
+console.log({
+        email: trimmedEmail,
+        message: trimmedMessage
+    });
+event.currentTarget.reset();
+localStorage.removeItem(STORAGE_KEY);
+formData.email = "";
+formData.message = "";
+}
